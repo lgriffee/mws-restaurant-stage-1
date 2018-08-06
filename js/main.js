8ff7818f7d078de836831a066d2923a0c2982355
Laura Griffee <laura@mailzone.com>
2018-08-05 18:21:34 -0600

    feat: Implement responsive design on homepage

--- js/main.js
+++ js/main.js
@@ -143,28 +143,36 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 // Create restaurant HTML
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  const div = document.createElement('div');
+  const textDiv = document.createElement('div');
+
+  textDiv.className = 'restaurant-text';
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  div.append(image);
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  textDiv.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  textDiv.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  textDiv.append(address);
+
+  div.append(textDiv);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  div.append(more);
+
+  li.append(div);
 
   return li
 }
